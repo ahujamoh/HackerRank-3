30be92de62439185aa5df72ef6b020c42c8b5335
Brian Oldfield <brian.oldfield@gmail.com>
2012-08-31 08:07:43 -0700

    Added information about level02.challenge02 (Bidding War).

---
+++ level02/challenge01/python_solution.py
@@ -0,0 +1,31 @@
+"""
+The seed code provided by HackerRank
+#!/bin/python
+
+def calculate_bid(player,pos,first_moves,second_moves):
+    #your logic here
+    return 0
+
+#gets the id of the player
+player = input()
+
+scotch_pos = input()         #current position of the scotch
+
+first_moves = [int(i) for i in raw_input().split()]
+second_moves = [int(i) for i in raw_input().split()]
+bid = calculate_bid(player,scotch_pos,first_moves,second_moves)
+print bid
+"""
+#!/bin/python
+
+def calculate_bid(player,pos,first_moves,second_moves):
+    return 0
+
+player = input()
+
+scotch_pos = input()         #current position of the scotch
+
+first_moves = [int(i) for i in raw_input().split()]
+second_moves = [int(i) for i in raw_input().split()]
+bid = calculate_bid(player,scotch_pos,first_moves,second_moves)
+print bid
